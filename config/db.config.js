0e49e79149029db83a4b25318bc7cd258708a40b
Semir Cohovic <semircohovic@gmail.com>
2022-03-15 18:05:50 +0100

    redis mongo config

--- config/db.config.js
+++ config/db.config.js
@@ -7,34 +7,34 @@ const {
 } = require("./config");
 
 // Replace this with your MONGOURI.
-const MONGOURI = `mongodb://127.0.0.1:27017/backend-qr`;
+// const MONGOURI = `mongodb://127.0.0.1:27017/backend-qr`;
 
-const InitiateMongoServer = async () => {
-  try {
-    await mongoose.connect(MONGOURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to DB !!");
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-};
-
-// const MONGOURI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/backend-qr`;
-
-// const InitiateMongoServer = () => {
-//   mongoose
-//     .connect(MONGOURI, {
+// const InitiateMongoServer = async () => {
+//   try {
+//     await mongoose.connect(MONGOURI, {
 //       useNewUrlParser: true,
 //       useUnifiedTopology: true,
-//     })
-//     .then(() => console.log("succesfuly connected to DB"))
-//     .catch((e) => {
-//       console.log(e);
-//       setTimeout(InitiateMongoServer, 5000);
 //     });
+//     console.log("Connected to DB !!");
+//   } catch (e) {
+//     console.log(e);
+//     throw e;
+//   }
 // };
+
+const MONGOURI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/backend-qr`;
+
+const InitiateMongoServer = () => {
+  mongoose
+    .connect(MONGOURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("succesfuly connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(InitiateMongoServer, 5000);
+    });
+};
 
 module.exports = InitiateMongoServer;
--- config/redis.config.js
+++ config/redis.config.js
@@ -1,9 +1,16 @@
 const redis = require("redis");
+const { REDIS_PORT, REDIS_URL } = require("./config");
 
-const redis_client = redis.createClient(
-  process.env.REDIS_PORT,
-  process.env.REDIS_HOST
-);
+// const redis_client = redis.createClient(
+//   process.env.REDIS_PORT,
+//   process.env.REDIS_HOST
+// );
+
+// redis_client.on("connect", function () {
+//   console.log("redis client connected");
+// });
+
+const redis_client = redis.createClient(REDIS_PORT, REDIS_URL);
 
 redis_client.on("connect", function () {
   console.log("redis client connected");
